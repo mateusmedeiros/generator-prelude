8196b18c4a346bf7831b5b5f3b1aa2c5c374d508
Mateus "Doodad" Medeiros <mateusbmedeiros@outlook.com>
2016-10-22 02:57:17 +0000

    Small fixes on App.js
    Fix the two import lines for react-router stuff and put the Root
    component in the '/' route.

---
+++ generators/app/templates/app/client/js/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Root extends React.Component {
+  render() {
+    return (
+      <div className="site-wrapper">
+        <h1> Hello </h1>
+      </div>
+    );
+  }
+}
+
+export default Root;
